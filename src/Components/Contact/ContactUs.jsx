c6b2c737cd72497a1743f52152858d1752abc593
jannatul_axsa <jannatulaxajanifa586@gmail.com>
2023-11-09 19:54:30 +0600

    add contact us section

--- src/Components/Contact/ContactUs.jsx
+++ src/Components/Contact/ContactUs.jsx
@@ -1,60 +1,52 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 const ContactUs = () => {
-    const Cardstyle = {
-        with:'300px',
-        height: '300px'
-    }
-    return (
-        <div className='mx-auto  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-3'>
+  return <>
+  <h2 className="text-2xl text-center font-sans font-thin mt-6 ">
+        Contact with <span className="text-[#BA8A3E] ">Us</span>{" "}
+      </h2>
+
+        <div className='mx-auto w-[90%] mb-20 mt-10  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-3'>
              <Helmet>
                
                <title>Contact Page</title>
                
            </Helmet>
-            <div className=''>
-            <div className="card  bg-primary text-primary-content" style={Cardstyle}>
+           
+
+
+            
+          
+            <div className="card   text-primary-content rounded-none border border-[#BA8A3E] bg-none " >
                 <div className="card-body">
                     <h2 className="card-title"></h2>
-                    <h2 className='text-xl font-bold'>Name: Johan</h2>
-                    <p>Phone:+1405551572</p>
-                     <p>Email: email1@johan.com</p>
-                    <div className="card-actions justify-end">
-                       
-                    </div>
+                    <h2 className='text-xl text-[#BA8A3E] font-bold'> Jannatul Axsa Janifa</h2>
+                    <p className='text-[#BA8A3E]'>Phone:+1405551572</p>
+                     <p className='text-[#BA8A3E]'>Email: jannatulaxajanifa586"gmail.com.com</p>
                 </div>
             </div>
-            </div>
-            <div>
-            <div className="card  bg-primary text-primary-content" style={Cardstyle}>
+            <div className="card   text-primary-content rounded-none border border-[#BA8A3E] bg-none " >
                 <div className="card-body">
                     <h2 className="card-title"></h2>
-                    <h2 className='text-xl font-bold'>Name: Jon</h2>
-                    <p>Phone:+1405551572</p>
-                     <p>Email: email1@jon.com</p>
-                    <div className="card-actions justify-end">
-                        
-                    </div>
+                    <h2 className='text-xl text-[#BA8A3E] font-bold'> Jannatul Axsa Janifa</h2>
+                    <p className='text-[#BA8A3E]'>Phone:+1405551572</p>
+                     <p className='text-[#BA8A3E]'>Email: jannatulaxajanifa586"gmail.com.com</p>
                 </div>
             </div>
-            </div>
-            <div>
-            <div className="card  bg-primary text-primary-content" style={Cardstyle}>
+            <div className="card   text-primary-content rounded-none border border-[#BA8A3E] bg-none " >
                 <div className="card-body">
                     <h2 className="card-title"></h2>
-                    <h2 className='text-xl font-bold'>Name: Creblin</h2>
-                     <p>Phone:+1405551572</p>
-                     <p>Email: email1@crebline.com</p>
-                    <div className="card-actions justify-end">
-                       
-                    </div>
+                    <h2 className='text-xl text-[#BA8A3E] font-bold'> Jannatul Axsa Janifa</h2>
+                    <p className='text-[#BA8A3E]'>Phone:+1405551572</p>
+                     <p className='text-[#BA8A3E]'>Email: jannatulaxajanifa586"gmail.com.com</p>
                 </div>
             </div>
-            </div>
+         
+            
+            
 
         </div>
-    );
+  </>;
 };
 
 export default ContactUs;
-
